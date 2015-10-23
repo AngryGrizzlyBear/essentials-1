e1ed7d0a72911d75ddac14e61a3d480975ead7a3
Taylor Bockman <taylor.bockman@gmail.com>
2015-10-23 16:57:34 -0700

    initial emacs config

---
+++ dotfiles/emacs.d/package.el
@@ -0,0 +1,18 @@
+(require 'cl)
+ 
+(defvar my-packages
+  '(ack-and-a-half clojure-mode coffee-mode paredit solarized-theme)
+  "Packages to make sure are installed")
+ 
+(defun my-packages-installed-p ()
+  (loop for p in my-packages
+        when (not (package-installed-p p)) do (return nil)
+        finally (return t)))
+ 
+(unless (my-packages-installed-p)
+  ;; check for new packages (package versions)
+  (package-refresh-contents)
+  ;; install the missing packages
+  (dolist (p my-packages)
+    (when (not (package-installed-p p))
+      (package-install p))))
