6570499c9d0f978d33be7622c83d1877bd9f095a
Taylor Bockman <tbockman@taylorbockman.com>
2015-11-05 23:07:07 -0800

    Add i3bar configuration and extras

--- scripts/sysinstall/install_packages.sh
+++ scripts/sysinstall/install_packages.sh
@@ -47,7 +47,7 @@ sudo pacman-key --lsign-key 962DDE58
 packages=("wget", "emacs", "gvim", "openssh", "steam", "zsh", "zsh-completions", "tmux",
 	        "xf86-video-fbdev", "xf86-video-intel", "xf86-video-vesa", "alsa-utils", "xorg",
           "xorg-xinit", "dmenu", "ttf-inconsolata", "i3", "google-chrome", "clojure", "leiningen",
-	        "xclip", "playerctl", "infinality-bundle", "lieningen-completions", "npm", "conky", "feh") 
+	        "xclip", "playerctl", "infinality-bundle", "lieningen-completions", "npm", "conky", "feh", "networkmanager") 
 
 # Reload the font cache
 fc-cache -fv
