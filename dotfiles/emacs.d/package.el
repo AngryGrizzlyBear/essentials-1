977e36dc55fc0d511e5d7c630f62ff398c87c619
Taylor Bockman <tbockman@taylorbockman.com>
2016-04-07 15:04:49 -0700

    stuff

--- dotfiles/emacs.d/package.el
+++ dotfiles/emacs.d/package.el
@@ -1,8 +1,8 @@
 (require 'cl)
 
 (defvar my-packages '(ag auto-complete clojure-mode coffee-mode css-mode circe expand-region framemove flycheck
-                         esh-help enh-ruby-mode flymake-ruby ruby-block robe inf-ruby ac-inf-ruby rspec-mode
-                         fill-column-indicator flycheck-clojure flycheck-haskell flycheck-rust slim-mode feature-mode
+                         esh-help enh-ruby-mode ess flymake-ruby ruby-block robe inf-ruby ac-inf-ruby rspec-mode
+                         fill-column-indicator flycheck-clojure flycheck-haskell flycheck-rust slim-mode
                          flycheck-pos-tip gitignore-mode golden-ratio haskell-mode swiper sass-mode linum-relative
                          js2-mode less-css-mode magit neotree paredit scala-mode2 scss-mode smex powerline toml-mode
                          projectile rust-mode color-theme-solarized wrap-region stylus-mode rainbow-delimiters
