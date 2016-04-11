230536db9659137eeae90984470ea7d4700acf05
Taylor Bockman <tbockman@taylorbockman.com>
2016-04-10 19:30:16 -0700

    Add gherkin major mode

--- dotfiles/emacs.d/package.el
+++ dotfiles/emacs.d/package.el
@@ -2,7 +2,7 @@
 
 (defvar my-packages '(ag auto-complete clojure-mode coffee-mode css-mode circe expand-region framemove flycheck
                          esh-help enh-ruby-mode ess flymake-ruby ruby-block robe inf-ruby ac-inf-ruby rspec-mode
-                         fill-column-indicator flycheck-clojure flycheck-haskell flycheck-rust slim-mode
+                         fill-column-indicator flycheck-clojure flycheck-haskell flycheck-rust slim-mode feature-mode
                          flycheck-pos-tip gitignore-mode golden-ratio haskell-mode swiper sass-mode linum-relative
                          js2-mode less-css-mode magit neotree paredit scala-mode2 scss-mode smex powerline toml-mode
                          projectile rust-mode color-theme-solarized wrap-region stylus-mode rainbow-delimiters
