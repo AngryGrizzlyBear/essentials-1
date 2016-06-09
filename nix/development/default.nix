ab76faaf11b75d446f64c7471a3630a1b4f12837
Taylor Bockman <tbockman@taylorbockman.com>
2016-06-09 15:17:34 -0700

    Ocaml files, nix development files

--- nix/development/default.nix
+++ nix/development/default.nix
@@ -1,8 +1,13 @@
 { config, pkgs, ... }:
 {
   environment = {
+
+    # OCaml has trouble finding pthread. So we set PTHREAD_LIBS to give iet a heads up
+    # ** currently doesnt set PTHREAD_LIBS at all **
     variables = {
       EDITOR = "nvim";
+      PTHREAD_CFLAGS = "-I${pkgs.glibc}/include/";
+      PTHREAD_LIBS = "-L${pkgs.glibc}/lib/";
     };
 
     systemPackages = with pkgs; [
@@ -14,6 +19,7 @@
       freetype                      # Library to render fonts required by some packages
       gcc                           # GNU Compiler Collection
       ghc                           # Haskell compiler
+      glibc                         # Glibc
       gnumake                       # GNU Make
       git                           # Source control
       libev                         # Event loop library
@@ -24,10 +30,12 @@
       ocamlPackages.ocaml_batteries # OCaml batteries included
       ocamlPackages.merlin          # OCaml editor enhancement
       ocamlPackages.findlib         # OCaml findlib
+      ocamlPackages.lambdaTerm      # Lambda Term Library
       ocamlPackages.ocaml_lwt       # OCaml LWT Library - Saves time otherwise spent fighting with opam
       ocamlPackages.ocaml_oasis     # OASIS Project Architecture Tool for OCaml
       ocamlPackages.utop            # Much better top-level for OCaml
       opam                          # OCaml package manager
+      pkgconfig
       python3                       # Python 3
       python34Packages.setuptools   # Python setuptools
       silver-searcher               # Better than grep
