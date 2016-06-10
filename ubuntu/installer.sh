e0b20c64ad1c1ad2186d1bc9d8162cb941b851f0
Taylor Bockman <tbockman@taylorbockman.com>
2016-06-09 17:44:37 -0700

    Create installer.sh

---
+++ ubuntu/installer.sh
@@ -0,0 +1,24 @@
+# Simple script to get an ubuntu system up and running
+
+# Pre-setup
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt-get update
+sudo apt-get upgrade
+
+# Window Manager
+sudo apt-get install xmonad
+
+# Dmenu
+sudo apt-get install suckless-tools
+
+# Languages
+sudo apt-get install python-dev python-pip python3-dev python3-pip
+
+# Editors
+sudo apt-get install vim
+sudo apt-get install neovim
+
+# Tools
+sudo apt-get install htop
+sudo apt-get install chromium-browser
+
