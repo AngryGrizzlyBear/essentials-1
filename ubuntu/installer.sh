100e18d75a8c7addc62e8f134fbb9d97c3d75cfe
Taylor Bockman <tbockman@taylorbockman.com>
2016-06-09 18:07:00 -0700

    Update installer.sh

--- ubuntu/installer.sh
+++ ubuntu/installer.sh
@@ -1,47 +1,62 @@
 # Simple script to get an ubuntu system up and running
 # Tested on Ubuntu 16.04 LTS
 
+echo "======== UBUNTU AUTO-INSTALL SCRIPT ========"
 # Pre-setup
+echo "Running pre-setup..."
 sudo add-apt-repository ppa:neovim-ppa/unstable
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get -y update
+sudo apt-get -y upgrade
 
 # Window Manager
-sudo apt-get install xmonad
-sudo apt-get install libghc-xmonad-contrib-dev
+echo "Installing window manager..."
+sudo apt-get -y install xmonad
+sudo apt-get -y install libghc-xmonad-contrib-dev
 
 # Dmenu
-sudo apt-get install suckless-tools
+echo "Installing Dmenu..."
+sudo apt-get -y install suckless-tools
 
 # Languages
-sudo apt-get install python-dev 
-sudo apt-get install python-pip 
-sudo apt-get install python3-dev 
-sudo apt-get install python3-pip
+echo "Installing languages..."
+sudo apt-get -y install python-dev 
+sudo apt-get -y install python-pip 
+sudo apt-get -y install python3-dev 
+sudo apt-get -y install python3-pip
 
 # Editors
-sudo apt-get install vim
-sudo apt-get install neovim
+echo "Installing editors..."
+sudo apt-get -y install vim
+sudo apt-get -y install neovim
 
 # Remove Unity (This has to be done here so we can reinstall what unity removes)
-sudo apt-get purge unity
+echo "Removing Unity..."
+sudo apt-get -y purge unity
 
 # Tools
-sudo apt-get install transmission
-sudo apt-get install htop
-sudo apt-get install chromium-browser
+echo "Installing tools..."
+sudo apt-get -y install transmission
+sudo apt-get -y install htop
+sudo apt-get -y install chromium-browser
 
 # Cleanup
-sudo apt-get purge firefox
-sudo apt-get purge rhythmbox
-sudo apt-get purge cheese
-sudo apt-get purge shotwell
-sudo apt-get purge shotwell-common
-sudo apt-get purge thunderbird
-sudo apt-get purge aisleriot
-sudo apt-get purge webbrowser-app
-sudo apt-get purge gnome-sudoku
-sudo apt-get purge mahjongg
-sudo apt-get purge gnomine
-sudo apt-get purge ace-of-penguins
-sudo apt-get purge onboard
+echo "Cleaning up unneccessary programs..."
+sudo apt-get -y purge firefox
+sudo apt-get -y purge rhythmbox
+sudo apt-get -y purge cheese
+sudo apt-get -y purge shotwell
+sudo apt-get -y purge shotwell-common
+sudo apt-get -y purge thunderbird
+sudo apt-get -y purge aisleriot
+sudo apt-get -y purge webbrowser-app
+sudo apt-get -y purge gnome-sudoku
+sudo apt-get -y purge mahjongg
+sudo apt-get -y purge gnomine
+sudo apt-get -y purge ace-of-penguins
+sudo apt-get -y purge onboard
+sudo apt -y autoremove
+
+echo "!!! DONE INSTALLING !!!"
+echo "======== REBOOTING IN 30 SECONDS ========"
+sleep 30
+sudo reboot
